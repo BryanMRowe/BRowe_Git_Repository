38b91ef1531f489de91a16c07556b58703e2d79a
dsab84 <32781524+dsab84@users.noreply.github.com>
2018-07-16 17:04:03 -0400

    Update main.py

--- Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
+++ Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
@@ -1,7 +1,7 @@
 # This script combs through delimited text files to find and extract ground control points
 
 # import dependencies
-import csv
+import os, csv
 
 
 
@@ -12,4 +12,10 @@ import csv
 # open/read text files
 with open(control_file_path, "r") as control_file:
     control_points = control_file.read()
+	#sort into ID,X,Y,Z,DESCRIPTION
+	#control_points_sorted = control_points.sort blah blah blah
+	
     #append to control point collection
+	all_control = open("all_control.csv","a")
+	all_control.write(control_points_sorted)
+	all_control.close
