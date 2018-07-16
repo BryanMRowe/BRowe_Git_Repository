a99ed313c5099d76884ea302febae6a5aa272a88
dsab84 <32781524+dsab84@users.noreply.github.com>
2018-07-16 17:09:16 -0400

    Update main.py

--- Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
+++ Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
@@ -4,18 +4,22 @@
 import os, csv
 
 
+keywords = ["control", "ctrl"]
 
+directory = "B:\"
 
+result = {}  # dict store our results
 
-
-
-# open/read text files
-with open(control_file_path, "r") as control_file:
-    control_points = control_file.read()
-	#sort into ID,X,Y,Z,DESCRIPTION
-	#control_points_sorted = control_points.sort blah blah blah
+for filename in os.listdir(directory):
+    for keyword in keywords:
+        if keyword in filename:
+			# open/read text files
+			with open(control_file_path, "r") as control_file:
+				control_points = control_file.read()
+				#sort into ID,X,Y,Z,DESCRIPTION
+				#control_points_sorted = control_points.sort blah blah blah
 	
-    #append to control point collection
-	all_control = open("all_control.csv","a")
-	all_control.write(control_points_sorted)
-	all_control.close
+				#append to control point collection (csv, postGIS database, las, etc.)
+				all_control = open("all_control.csv","a")
+				all_control.write(control_points_sorted)
+				all_control.close
