576952443f3b301ba10ecf7786df8e0a532e9069
dsab84 <32781524+dsab84@users.noreply.github.com>
2018-07-16 16:04:41 -0400

    Update main.py

--- Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
+++ Projects/Analytics/Local/Map/Collect-SummarizeGroundControl/main.py
@@ -1,37 +1,15 @@
 # This script combs through delimited text files to find and extract ground control points
 
 # import dependencies
-import glob
 import csv
 
-download_files = glob.glob("*/Download/*.txt")
-for f in download_files1:
-    print(f)
 
 
 
-drive = "B:\"
-path_pt2 = x
-path_pt3 = os.path.join("data", "budget_data_1.csv")
-path = drive + client_job + 
 
 
-directory = os.fsencode(directory_in_str)
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".asm") or filename.endswith(".py"): 
-        # print(os.path.join(directory, filename))
-        continue
-    else:
-        continue
-
-# make data file paths cross-platform ready
-budget_data_1_path = os.path.join("data", "budget_data_1.csv")
-
-# open/read text files & print contents/type
-with open(budget_data_1_path, "r") as budget_data_1_file:
-    budget_data_1 = budget_data_1_file.read()
-    print(budget_data_1_file)
-    #print(budget_data_1)
-    #print(type(budget_data_1))
+# open/read text files
+with open(control_file_path, "r") as control_file:
+    control_points = control_file.read()
+    #append to control point collection
